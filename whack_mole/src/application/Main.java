fed00fe658df35d459bffedb6a9a2d72dbc52f09
lithrur <lithrur@localhost>
2025-03-28 13:39:52 -0400

    changed_Main

--- whack_mole/src/application/Main.java
+++ whack_mole/src/application/Main.java
@@ -3,7 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
-            BorderPane root = loader.load();
+            GridPane root = loader.load();
             Scene scene = new Scene(root, 400, 400);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Whack-a-Mole");
